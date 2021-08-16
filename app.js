f25085f6aed365f7d52ab003ed2b5e8b9e0b88cd
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-16 16:03:13 -0700

    working question 1

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
+import { countsAsAYes } from './utils.js';
 
+const button = document.getElementById('quiz');
+let correctAnswers = 0;
 // initialize state
 
 // set event listeners 
+button.addEventListener('click', ()=> {
+    let correctAnswers = 0;
+    const firstName = prompt ('Hi there! What is your first name?');
+    const lastName = prompt ('What is your last name?');
+    const wantsToContinue = confirm(`Okay ${firstName} ${lastName}, are you ready to take the quiz?`);
+
+
+    if (!wantsToContinue) return; 
+
+    const answer1 = prompt('question 1');
+    if (countsAsAYes) correctAnswers++;
+    console.log(correctAnswers);
+});
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ utils.js
@@ -0,0 +1,4 @@
+export function countsAsAYes(){
+    if (answer1.charAt(0).toLowerCase () === 'y') correctAnswers++;
+    console.log(correctAnswers)
+  }
