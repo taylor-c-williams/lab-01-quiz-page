7401578e2fed06f648a01be1ef9521c9ea23d55a
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-16 17:52:51 -0700

    pass test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,17 @@
 // IMPORT MODULES under test here:
+import { countsAsAYes } from '../utils.js';
 // import { example } from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test counts as a yes', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = countsAsAYes('yaas');
 
     //Expect
     // Make assertions about what is expected versus the actual result
