5ace7296de6f7d902b4efad68691ee9b359b9b3d
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-16 20:47:07 -0700

    final

--- app.js
+++ app.js
@@ -2,36 +2,39 @@
 import { countsAsAYes } from './utils.js';
 
 const button = document.getElementById('quiz');
+const complete = document.getElementById('complete');
+
 let correctAnswers = 0;
 // initialize state
 
 // set event listeners 
 button.addEventListener('click', ()=> {
 
-    const firstName = prompt ('Hi there! What is your first name?');
+    const firstName = prompt ('Hi there! What is your name?');
     if (!firstName) return;
     const wantsToContinue = confirm(`Okay ${firstName}, are you ready to take the quiz?`);
 
 
     if (!wantsToContinue) return; 
 
-    const answer1 = prompt('question 1');
+    const answer1 = prompt("Is the Elephant's Foot comprised of a material called Corium?");
     if (countsAsAYes(answer1)) correctAnswers++;
 
     console.log(correctAnswers);
 
-    const answer2 = prompt('question 2');
+    const answer2 = prompt("Is the Elephant's Foot radioactive?");
     if (countsAsAYes(answer2)) correctAnswers++;
 
     console.log(correctAnswers);
 
-    const answer3 = prompt('question 3');
-    if (countsAsAYes(answer3)) correctAnswers++;
+    const answer3 = prompt("Is the Elephant's foot the only known instance of Corium on the earth?");
+    if (!countsAsAYes(answer3)) correctAnswers++;
 
     console.log(correctAnswers);
 
-    alert('Your results are in.')
-    complete.textContent = `Okay, ${firstName} you got ${correctAnswers} out of 3!`;
+    alert(`Well done ${firstName}.
+  Tabulating results...`);
+    complete.textContent = `Congratulations, ${firstName} you got ${correctAnswers} out of 3 questions!`;
 });
 
 
